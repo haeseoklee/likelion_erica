b43931a78aeb5b3085b0cad08f2b3b7e06b2e311
haeruseok <interpret96@gmail.com>
2017-01-13 22:22:45 +0900

    modify settings base.py

--- likelion_erica/settings/base.py
+++ likelion_erica/settings/base.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'r7nae^3for2@_)m+5nthvej(m2iat(8w_r%5o6t71=n5jkojf_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-52-79-171-54.ap-northeast-2.compute.amazonaws.com',
+ALLOWED_HOSTS = [
+    '127.0.0.1', 'localhost', '127.0.0.1:8000', 'localhost:8000',
+    'ec2-52-79-171-54.ap-northeast-2.compute.amazonaws.com',
     '52.79.171.54',
     ]
 
