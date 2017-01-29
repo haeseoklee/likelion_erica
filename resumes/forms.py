9e30c5f7f2a3653c673f552c8850c70d91794aa6
haeruseok <interpret96@gmail.com>
2017-01-29 15:07:32 +0000

    modify forms.py and templates in home

--- resumes/forms.py
+++ resumes/forms.py
@@ -10,12 +10,12 @@ class ResumeForm(forms.ModelForm):
         FILE_TYPE_ONLY = ['pdf']
         try:
             document = self.cleaned_data['document']
-            if document:
-                file_type = str(document).split('.')[1]
-                if not file_type in FILE_TYPE_ONLY:
-                    raise forms.ValidationError("pdf 파일만 가능합니다.")
-                if document.name == '':
-                    raise forms.ValidationError("잘못된 파일 이름입니다.")
-            return self.cleaned_data
-        except:
-            raise forms.ValidationError("선택된 파일이 없습니다.")
+	except:
+	    raise forms.VlidationError("선택된 파일이 없습니다.")
+        if document:
+            file_type = str(document).split('.')[1]
+            if not file_type in FILE_TYPE_ONLY:
+                raise forms.ValidationError("pdf 파일만 가능합니다.")
+            if document.name == '':
+                raise forms.ValidationError("잘못된 파일 이름입니다.")
+        return self.cleaned_data
